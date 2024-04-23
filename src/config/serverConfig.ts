a2efc668faf207f9ea847a24576bb273551aabe6
Sanket Singh <singhsanket143@gmail.com>
2024-04-23 21:10:45 +0530

    Added eslint configurations

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    PORT: process.env.PORT || 3000
-}
+  PORT: process.env.PORT || 3000,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Express } from "express";
-import serverConfig from "./config/serverConfig";
 
+import serverConfig from "./config/serverConfig";
 const app: Express = express();
 
 app.listen(serverConfig.PORT, () => {
-    console.log(`Server started at *:${serverConfig.PORT}`);
+  console.log(`Server started at *:${serverConfig.PORT}`);
 });
