06d5269182fc9fbcba928755c2b04aab11b7fda8
khush <khush.suthar@gmail.com>
2024-05-02 16:57:51 +0530

    Added: BullBoard an Queue background jobs inspector

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
+import bullBoardAdapter from "./config/bullBoardConfig";
 
 
 const app: Express = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
-
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
+  
   SampleWorker('SampleQueue');
 
   sampleQueueProducer('SampleJob', {
