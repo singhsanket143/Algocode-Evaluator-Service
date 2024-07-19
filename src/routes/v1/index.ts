fb438b4a6f7ce6e734f1d96930a8aae716552a20
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-19 21:49:06 +0530

    Feat: implemented evaluation queue Infra

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,14 +1,11 @@
 import express from "express";
 
 import { pingCheck } from "../../controllers/pingController";
-import evaluationRouter from "./evaluationRoutes";
 import submissionRouter from "./submissionRoutes";
 
 const v1Router = express.Router();
 
 v1Router.use('/submissions', submissionRouter);
-v1Router.use('/evaluations', evaluationRouter);
-
 v1Router.get('/ping', pingCheck);
 
 export default v1Router;
